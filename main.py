5dcd483b82ef51ca6a741c79f558694459923b05
vladimir_pokorny <vladimir.pokorny@tescan.com>
2023-05-12 12:27:43 +0200

    little change

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ class MyApp(QWidget):
 
         self.files_with_path = files
 
+
         for i in range(len(files)):
             cleaned_filename = clean_file_name(files[i])
             self.list_widget.addItem(cleaned_filename)
